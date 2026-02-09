bb06173db41dae40c6569f68438f6a0d9851cc38
Khoa Le <dangkhoale176@gmail.com>
2026-02-08 23:02:26 -0500

    return indices of num make sum of target

--- twoSums.py
+++ twoSums.py
@@ -6,6 +6,6 @@ def twoSums(nums, target):
             return [seen[complement], i]
         seen[num] = i
         
-nums = [1,2,3,4,5,6,7]
+nums = [1,2,3,4,5]
 target = 5
 print(twoSums(nums,target))
